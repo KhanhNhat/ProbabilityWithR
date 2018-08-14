50139554b0d0cda40de934737c547cfc21d2381b
Nhat Hoang <hoangphamkhanhnhat@gmail.com>
2018-08-14 16:57:09 +1000

    Create a deck of cards Calculate probability of a simple event: picking an Ace from this deck of cards.

--- DiscreteProbability.R
+++ DiscreteProbability.R
@@ -17,3 +17,20 @@ beadTable = table(pickingBead)
 
 #And approximately probability
 prop.table(beadTable)
+
+#Create a desk of card
+suits = c('Diamonds', 'Clubs', 'Hearts', 'Spades')
+numbers = c('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+deckCardsDF = expand.grid(number = numbers, suit = suits)
+deckCards = str_c(deckCardsDF$number, deckCardsDF$suit, sep = ' ')
+
+#Now calculate a probability of picking an Ace from this deck
+#Get each card from the deck, check if it is in a group of Aces
+#The result is a vector of TRUE and FALSE value
+#Mean of this vector = num of TRUE/num of TRUE and FALSE
+#Therefore, it is also probability of picking an Ace
+mean(deckCards %in% c("A Diamonds", "A Hearts", "A Clubs", "A Spades"))
+
+
+
+
